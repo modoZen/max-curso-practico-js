56e042874d53da81f8b555279fcad31a84db6964
Max <mdiazn@uni.pe>
2022-04-29 22:25:10 -0500

    clase13

--- descuentos/app.js
+++ descuentos/app.js
@@ -1,8 +1,5 @@
-const inputPrice = document.getElementById('InputPrice');
-const inputDiscount = document.getElementById('InputDiscount');
-
-
 function calcularPrecioConDescuento(precio, descuento) {
+    console.log(descuento)
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
@@ -10,11 +7,34 @@ function calcularPrecioConDescuento(precio, descuento) {
 }
 
 function onClickButtonPriceDiscount() {
+    const cupons = [
+        {
+            name: 'maxter',
+            discount: 25
+        },
+        {
+            name: 'mux',
+            discount:35
+        }
+    ]
+    const inputPrice = document.getElementById('InputPrice');
     const priceValue = inputPrice.value;
-    const discountValue = inputDiscount.value;
-    const precioConDescuento = calcularPrecioConDescuento(priceValue,discountValue);
+    // const inputDiscount = document.getElementById('InputDiscount');
+    // const discountValue = inputDiscount.value;
+    const inputCupon = document.getElementById('InputCupon');
+    const cuponValue = inputCupon.value;
+    const cupon = cupons.find(cupon=> cupon.name === cuponValue)
+    const avisoP = document.getElementById('AvisoP');
     const resultP = document.getElementById('ResultP');
-    resultP.innerText = `El precio con descuento es: $${precioConDescuento}`;
+    if(!cupon){
+        avisoP.innerText = cuponValue!=''?`Cupon invalido`:'';
+        const precioConDescuento = calcularPrecioConDescuento(priceValue,0 );
+        resultP.innerText = `El precio con descuento es: $${precioConDescuento}`;
+    } else {
+        avisoP.innerText = ``;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue,cupon.discount);
+        resultP.innerText = `El precio con descuento es: $${precioConDescuento}`;
+    }
 }
 
 
