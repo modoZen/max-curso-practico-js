78e438431dfef913961e8b22c9b2d3738eb52684
Max <mdiazn@uni.pe>
2022-04-29 21:09:42 -0500

    clase11

--- descuentos/app.js
+++ descuentos/app.js
@@ -1,1 +1,6 @@
-console.warn('holi')
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
